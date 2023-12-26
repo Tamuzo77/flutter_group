c446161d51334675dd3a2e8dfcb96339268f6d51
Tamuzo77 <tamuzomenga@gmail.com>
2023-12-26 16:03:58 +0100

    Declaration des couleurs

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromRGBO(0, 66, 239, 1.0);
+const bgDark = Color.fromRGBO(10, 18, 23, 0.9294117647058824);
+const textGreyColor = Color.fromRGBO(63, 59, 59, 1.0);
